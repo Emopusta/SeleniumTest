31bbc79c5e469c32ed3d0eef2eca96a752f31968
Emopusta <emreduman225@hotmail.com>
2022-12-25 19:57:23 +0300

    Add project files.

---
+++ SeleniumTest/CommandTests/Images/ImageTests.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest.CommandTests.Images
+{
+    public class ImageTests
+    {
+        IWebDriver _webDriver;
+
+        public ImageTests(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+
+        public string POSTImageTest(int id, string filePath)
+        {
+            System.Threading.Thread.Sleep(1000);
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div/button")).Click();
+            System.Threading.Thread.Sleep(1000);
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[1]/div[2]/button")).Click();
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[2]/div/table/tbody/tr/td[2]/input")).Clear();
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[2]/div/table/tbody/tr/td[2]/input")).SendKeys(id.ToString());
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[3]/div[2]/div/table/tbody/tr/td[2]/div/input")).SendKeys(filePath);
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[2]/button")).Click();
+            var result = _webDriver.FindElement(By.XPath("//*[@id=\"post_api_Images_responses\"]/tbody/tr/td[1]")).Text;
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[1]/div[2]/button[1]")).Click();
+            _webDriver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div/button")).Click();
+            return result;
+        }
+    }
+}
---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTest.CommandTests.Images;
+
+IWebDriver driver = new ChromeDriver();
+
+// Navigate to the website
+driver.Navigate().GoToUrl("https://localhost:7096/swagger/index.html");
+
+// Find the search box element and enter a query
+IList<string> results= new List<string>();
+
+ImageTests imageTests = new ImageTests(driver);
+
+results.Add(imageTests.POSTImageTest(1, "C:\\Users\\emred\\Pictures\\asd.PNG"));
+results.Add(imageTests.POSTImageTest(1, "C:\\Users\\emred\\Pictures\\asd.PNG"));
+results.Add(imageTests.POSTImageTest(1, "C:\\Users\\emred\\Pictures\\asd.PNG"));
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
+
+
+//System.Threading.Thread.Sleep(1000);
+//driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div/button")).Click();
+//System.Threading.Thread.Sleep(1000);
+//driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[1]/div[2]/button")).Click();
+//driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[2]/div/table/tbody/tr/td[2]/input")).SendKeys("1");
+//driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[3]/div[2]/div/table/tbody/tr/td[2]/div/input")).SendKeys("C:\\Users\\emred\\Pictures\\asd.PNG");
+//driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[2]/button")).Click();
+//var item = driver.FindElement(By.XPath("//*[@id=\"post_api_Images_responses\"]/tbody/tr/td[1]")).Text;
+
+
+
+// Find the search button and click it
+//IWebElement searchButton = driver.FindElement(By.XPath("//*[@id=\"operations-Images-post_api_Images\"]/div[2]/div/div[1]/div[1]/div[2]/button"));
+//searchButton.Click();
